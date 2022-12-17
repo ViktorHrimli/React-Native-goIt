aeaa61218a8b97f31b032f4993a615233b6cda87
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-12-17 22:05:48 +0200

    hw-02

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImageBackground,
   View,
   Text,
   TextInput,
   StyleSheet,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
 } from "react-native";
 
 const Login = () => {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const handleFocuse = () => {
+    setIsShowKeyboard(true);
+  };
+
   return (
     <ImageBackground
       source={require("../assets/img/PhotoBG.jpg")}
       style={styles.image}
     >
-      <View style={styles.conteiner}>
-        <Text style={styles.title}>Войти</Text>
+      <View style={{ ...styles.conteiner, flex: isShowKeyboard ? 0.7 : 0.6 }}>
+        <Text style={styles.title}>Login</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View style={styles.form}>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Email or Phone"
+                onFocus={handleFocuse}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry={true}
+                onFocus={handleFocuse}
+              />
+            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Send</Text>
+            </TouchableOpacity>
+            <View style={{ alignItems: "center", marginTop: 10 }}>
+              <Text style={{ color: "#1B4371", fontSize: 18 }}>Sign Up?</Text>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </ImageBackground>
   );
@@ -31,18 +67,44 @@ const styles = StyleSheet.create({
   conteiner: {
     flex: 0.6,
     flexDirection: "column",
-    alignItems: "center",
     justifyContent: "flex-start",
     backgroundColor: "#fff",
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
-    padding: 32,
+    padding: 16,
   },
   title: {
-    fontSize: "30",
-    lineHeight: "35",
+    fontSize: 32,
+    lineHeight: 35,
     textAlign: "center",
     letterSpacing: 0.01,
     color: "#212121",
+  },
+  form: {
+    marginTop: 33,
+    marginBottom: 20,
+    marginHorizontal: 20,
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    borderColor: "#e8e8e8",
+    color: "#212121",
+    padding: 10,
+    marginBottom: 20,
+    textAlign: "center",
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    marginTop: 23,
+    padding: 16,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
   },
 });
